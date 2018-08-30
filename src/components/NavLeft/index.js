519a91f702acccb339c878ce7b0cff82167eb5fb
lvpeng <731010527@qq.com>
2018-08-30 23:24:46 +0800

    一，在public这个存储编译好的文件的的文件夹里建立assets文件夹，静态资源图片放这里。图片路径是/assets/****.png, 二，添加左侧Menu菜单组件

--- src/components/NavLeft/index.js
+++ src/components/NavLeft/index.js
@@ -1,7 +1,33 @@
 import React from 'react'
+import MentConfig from './'
+import { Menu, Icon } from 'antd';
+import MenuConfig from './../../config/menuConfig'
+// ./index.less 在const SubMenu = Menu.SubMenu 之前
+import './index.less'
+const SubMenu = Menu.SubMenu;
+const MenuItemGroup = Menu.ItemGroup;
 
 export default class NavLeft extends React.Component{
+    
     render(){
-        return <div>NavLeft</div>
+        function handleClick(e) {
+            console.log('click', e);
+          }
+        return (
+        <div>
+            <div className="logo">
+                <img src="/assets/logo-ant.svg" alt=""/>
+                <h1>Imooc MS</h1>
+            </div>
+            <Menu theme="dark">
+                <SubMenu key="sub1" title={<span><Icon type="mail" /><span>Navigation One</span></span>}>
+                    <Menu.Item key="1">Option 1</Menu.Item>
+                    <Menu.Item key="2">Option 2</Menu.Item>
+                    <Menu.Item key="3">Option 3</Menu.Item>
+                    <Menu.Item key="4">Option 4</Menu.Item>
+                </SubMenu>
+            </Menu>
+        </div>
+        )
     }
 }
