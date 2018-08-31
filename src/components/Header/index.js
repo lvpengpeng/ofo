4fea74555b328e64388cee44852c990346676491
lvpeng <731010527@qq.com>
2018-08-31 12:27:48 +0800

    新建utils文件夹，添加utils.js,放项目里常用的工具类函数。并默认导出

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,11 +2,19 @@
 import React from 'react'
 import {Row,Col} from 'antd'
 import './index.less'
+import Util from'../../utils/utils'
 export default class Header extends React.Component{
     componentWillMount(){
         this.setState({
             username:"lvpeng"
         })
+        setInterval(()=>{
+            let sysTime = Util.formateDate(new Date().getTime());
+            // 现在的系统时间=util里面的工具方法传入的时间戳返回
+            this.setState({
+                sysTime
+            })
+        },1000)
     }
     render(){
         return <div className="header">
@@ -19,7 +27,7 @@ export default class Header extends React.Component{
                 <Row className="breadcrumb">
                     <Col span="4">首页</Col>
                     <Col span="20" className="weather">
-                        <span className="date">2018-05-06</span>
+                        <span className="date">{this.state.sysTime}</span>
                         <span className="weather-tetail">请转多云</span>
                     </Col>
                 </Row>
