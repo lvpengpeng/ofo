079aa20a71932d4de7cb6ba471f767e18c1254f5
lvpeng <731010527@qq.com>
2018-08-31 12:06:12 +0800

    header布局

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,7 +1,28 @@
-import React from 'react'
 
+import React from 'react'
+import {Row,Col} from 'antd'
+import './index.less'
 export default class Header extends React.Component{
+    componentWillMount(){
+        this.setState({
+            username:"lvpeng"
+        })
+    }
     render(){
-        return <div>footer</div>
+        return <div className="header">
+                <Row className="header-top">
+                    <Col span="24">
+                        <span>欢迎，{this.state.username}</span>
+                        <a href="">退出</a>
+                    </Col>
+                </Row>
+                <Row className="breadcrumb">
+                    <Col span="4">首页</Col>
+                    <Col span="20" className="weather">
+                        <span className="date">2018-05-06</span>
+                        <span className="weather-tetail">请转多云</span>
+                    </Col>
+                </Row>
+            </div>
     }
 }
