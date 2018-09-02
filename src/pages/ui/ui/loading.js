323c9b9d4f325ea91e1564286d62c30746202f32
lvpeng <731010527@qq.com>
2018-09-02 23:22:58 +0800

    Spin可以保住任何组件，显示loading效果

--- src/pages/ui/ui/loading.js
+++ src/pages/ui/ui/loading.js
@@ -4,6 +4,7 @@ import './ui.less'
 export default class Loadings extends React.Component{
     render(){
         const icon = <Icon  type="loading" style={{fontSize:24}}/>
+        const iconLoading = <Icon type="loading" style={{ fontSize: 24 }} />
         return(
             <div>
                 <Card title="Spin用法" className="card-wrap">
@@ -14,6 +15,37 @@ export default class Loadings extends React.Component{
                     <Spin indicator={icon} style={{ marginLeft: 10 }} spinning={true}/>
                     {/* indicator 替换的是静态图片 ，不能旋转 ，除非换成gif动图 */}
                     {/* marginLeft: 10   一个数字，可不写单位 */}
+                </Card>
+                <Card title="内容遮罩" className="card-wrap">
+                    <Alert
+                        message="React"
+                        description="欢迎来到React高级实战课程"
+                        type="info"
+                        style={{ marginBottom: 10 }}
+                    />
+                    <Spin>
+                        <Alert
+                            message="React"
+                            description="欢迎来到React高级实战课程"
+                            type="warning"
+                            style={{ marginBottom: 10 }}
+                        />
+                    </Spin>
+                    <Spin tip="加载中。。。">
+                        <Alert
+                            message="React"
+                            description="欢迎来到React高级实战课程"
+                            type="warning"
+                            style={{ marginBottom: 10 }}
+                        />
+                    </Spin>
+                    <Spin indicator={iconLoading}>
+                        <Alert
+                            message="React"
+                            description="欢迎来到React高级实战课程"
+                            type="warning"
+                        />
+                    </Spin>
                 </Card>
             </div>
         )
