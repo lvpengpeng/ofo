0349c03f9b55df6245db8d7a8a10cad688a1a699
lvpeng <731010527@qq.com>
2018-09-04 16:35:15 +0800

    render对于表单的字典进行映射

--- src/pages/table/basicTable.js
+++ src/pages/table/basicTable.js
@@ -11,8 +11,8 @@ class BasicTable extends React.Component{
             {
                 id:'0',
                 userName:'Jack',
-                sex:'1',
-                state:'1',
+                sex:'2',
+                state:'4',
                 interest:'1',
                 birthday:'2000-01-01',
                 address:'北京市海淀区奥林匹克公园',
@@ -22,8 +22,8 @@ class BasicTable extends React.Component{
                 id: '1',
                 userName: 'Tom',
                 sex: '1',
-                state: '1',
-                interest: '1',
+                state: '2',
+                interest: '2',
                 birthday: '2000-01-01',
                 address: '北京市海淀区奥林匹克公园',
                 time: '09:00'
@@ -32,8 +32,8 @@ class BasicTable extends React.Component{
                 id: '2',
                 userName: 'Lily',
                 sex: '1',
-                state: '1',
-                interest: '1',
+                state: '3',
+                interest: '3',
                 birthday: '2000-01-01',
                 address: '北京市海淀区奥林匹克公园',
                 time: '09:00'
@@ -75,15 +75,41 @@ class BasicTable extends React.Component{
             },
             {
                 title:'性别',
-                dataIndex:'sex'
+                dataIndex:'sex',
+                render(sex){
+                    return sex==1?"男":"女"
+                }
             },
             {
                 title: '状态',
-                dataIndex: 'state'
+                dataIndex: 'state',
+                render(state){
+                    let config  = {
+                        '1':'咸鱼一条',
+                        '2':'风华浪子',
+                        '3':'北大才子',
+                        '4':'百度FE',
+                        '5':'创业者'
+                    }
+                    return config[state];
+                }
             },
             {
                 title:'兴趣',
-                dataIndex:'interest'
+                dataIndex:'interest',
+                render(abc) {
+                    let config = {
+                        '1': '游泳',
+                        '2': '打篮球',
+                        '3': '踢足球',
+                        '4': '跑步',
+                        '5': '爬山',
+                        '6': '骑行',
+                        '7': '桌球',
+                        '8': '麦霸'
+                    }
+                    return config[abc];
+                }
             },
             {
                 title: '生日',
